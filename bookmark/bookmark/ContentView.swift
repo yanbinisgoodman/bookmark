9f636a336215d23348f1ff18889c4c3d0ecdd934
Megan Calverley <calvem2@cs.washington.edu>
2021-05-22 13:39:20 -0700

    Fetch from NYT API

--- bookmark/bookmark/ContentView.swift
+++ bookmark/bookmark/ContentView.swift
@@ -8,10 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+    @ObservedObject var networkManager = NetworkManager()
+    init()  {
+//        print(networkManager.books)
     }
+    var body: some View {
+        NavigationView {
+//            List($networkManager.books.results.identified(by: \<#Root#>.id)) { movie in
+//                Text("\(movie.title)")
+////              NavigationLink(destination: MovieDetails(movie: movie)){
+////                    MovieRow(movie: movie)
+//
+////                }
+//            }
+            if networkManager.loading {
+                                Text("Loading ...")
+                            } else {
+                                List(networkManager.movies.results, id: \.title) { quiz in
+                                    Text(quiz.title)
+                                }
+                            }
+            
+            //3.
+//            List(comments) { comment in
+//                VStack(alignment: .leading) {
+//                    Text(comment.name)
+//                        .font(.title)
+//                        .fontWeight(.bold)
+//                    Text(comment.email)
+//                        .font(.subheadline)
+//                        .fontWeight(.bold)
+//                    Text(comment.body)
+//                        .font(.body)
+//                }
+                
+//        }
+//        //2.
+//        .onAppear() {
+////            apiCall().getUserComments { (comments) in
+////                self.comments = comments
+////            }
+//        }.navigationTitle("Comments")
+    }
+}
 }
 
 struct ContentView_Previews: PreviewProvider {
