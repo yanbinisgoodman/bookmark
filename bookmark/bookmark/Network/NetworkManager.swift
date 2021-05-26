2998382f78fc59c5fba12c0e8e12f5fa7b6455bb
Megan Calverley <calvem2@cs.washington.edu>
2021-05-25 17:20:22 -0700

    add next() functionality to load more books

--- bookmark/bookmark/Network/NetworkManager.swift
+++ bookmark/bookmark/Network/NetworkManager.swift
@@ -14,14 +14,21 @@ class NetworkManager: ObservableObject {
     @Published var loading = false
     private let api_key = "dbOUHC2GNZmbnGRzwbws2F3syLE7Wtge"
     private let api_url_base = "https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?"
-    private let offset = 0 // increments of 20; determines which 20 results of total to return
+    private var offset = 0 // increments of 20; determines which 20 results of total to return
+    private let numResults = 33672
 
     init() {
         loading = true
-        loadDataNormal()
+        next()
     }
     
-    private func loadDataNormal() {
+    // Returns true if there are more books to fetch
+    private func hasNext() -> Bool {
+        return offset < numResults
+    }
+    
+    // Loads next set of books
+    private func next() {
         guard let url = URL(string: "\(api_url_base)offset=\(offset)&api-key=\(api_key)") else { return }
         URLSession.shared.dataTask(with: url){ (data, _, _) in
 
@@ -31,6 +38,7 @@ class NetworkManager: ObservableObject {
             DispatchQueue.main.async {
                 self.books = books
                 self.loading = false
+                self.offset += 20
             }
         }.resume()
     }
