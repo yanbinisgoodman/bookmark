0f3cae8528356f4e06a609f48712fa230f79f601
Megan Calverley <calvem2@cs.washington.edu>
2021-05-31 17:21:53 -0700

    Add swipe behavior

---
+++ bookmark/bookmark/Views/CardView.swift
@@ -0,0 +1,101 @@
+//
+//  CardView.swift
+//  bookmark
+//
+//  Created by Megan on 5/31/21.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    @State private var translation: CGSize = .zero
+    private var thresholdPercentage: CGFloat = 0.5
+    
+    var book: Book
+    var id: Int
+    private var onRemove: (_ id: Int, _ book: Book, _ swipedRight: Bool) -> Void
+    
+    
+    init(id: Int, book: Book, onRemove: @escaping (_ id: Int, _ book: Book, _ swipedRight: Bool) -> Void) {
+//        self.id = id
+//        self.book = book
+        self.id = id
+        self.book = book
+        self.onRemove = onRemove
+    }
+    
+    // percentage of our own width have we swipped
+    // - Parameters:
+    //   - geometry: The geometry
+    //   - gesture: The current gesture translation value
+    private func getGesturePercentage(_ geometry: GeometryProxy, from gesture: DragGesture.Value) -> CGFloat {
+        gesture.translation.width / geometry.size.width
+    }
+    
+    var body: some View {
+        GeometryReader { geometry in
+            VStack(alignment: .leading) {
+                // Title card
+                // TODO: use more colors
+                Color("Color4")
+                    .overlay(
+                        VStack {
+                            Text(self.book.title)
+                            Text(self.book.author)
+                        }
+                        .frame(width: geometry.size.width, height: geometry.size.height * 0.75)
+                            .clipped()
+                    )
+                
+                HStack {
+                    VStack(alignment: .leading, spacing: 6) {
+                        Text(self.book.title)
+                            .font(.title)
+                            .bold()
+                        Text(self.book.author)
+                            .font(.subheadline)
+                            .bold()
+                        Text(self.book.ranks_history.count > 0 ? self.book.ranks_history[0].list_name : "")
+                            .font(.subheadline)
+                            .foregroundColor(.gray)
+                    }
+                }.padding(.horizontal)
+            }
+            .padding(.bottom)
+            .background(Color.white)
+            .cornerRadius(10)
+            .shadow(radius: 5)
+            // swipe behavior
+            .animation(.interactiveSpring())
+            .offset(x: self.translation.width, y: 0)
+            .rotationEffect(.degrees(Double(self.translation.width / geometry.size.width) * 25), anchor: .bottom)
+            .gesture(
+                DragGesture()
+                    .onChanged { value in
+                        self.translation = value.translation
+                    }.onEnded { value in
+                        // determine snap distance > half the width of the screen
+                        if abs(self.getGesturePercentage(geometry, from: value)) > self.thresholdPercentage {
+//                            print(self.getGesturePercentage(geometry, from: value))
+                            self.onRemove(self.id, self.book, self.getGesturePercentage(geometry, from: value) > 0)
+                        } else {
+                            self.translation = .zero
+                        }
+                    }
+            )
+        }
+    }
+}
+
+// 4
+//struct CardView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        CardView(book: Book(title: "Mark", description: "Bennett", author: "Ash", price: "27", age_group: <#T##String?#>: "12", publisher: "person_1", ranks_history: <#T##[RankHistory]#>: "Insurance Agent"),
+//                 onRemove: { _ in
+//                    // do nothing
+//            })
+//            .frame(height: 400)
+//            .padding()
+//    }
+//    }
+//}
--- bookmark/bookmark/Views/HomeView.swift
+++ bookmark/bookmark/Views/HomeView.swift
@@ -8,15 +8,90 @@
 import SwiftUI
 
 struct HomeView: View {
+    // contains book results
     @ObservedObject var networkManager = NetworkManager()
+    @State var removedIndices: [Int] = []
+    @State var bookList: [Book] = []
+    @State var currentIndex: Int = 19
+    
+//    init() {
+//        books = networkManager.books.results
+//    }
 
+    // Return the CardView's width given its offset in the array
+    // - Parameters:
+    //   - geometry: The geometry proxy of the parent
+    //   - id: The ID of the current card
+    private func getCardWidth(_ geometry: GeometryProxy, id: Int) -> CGFloat {
+        let books = networkManager.books.results
+        let offset: CGFloat = CGFloat(books.count - removedIndices.count - 1 - id) * 20
+        return geometry.size.width - offset
+    }
+    
+    // Return the CardView's frame offset given its offset in the array
+    // - Parameters:
+    //   - geometry: The geometry proxy of the parent
+    //   - id: The ID of the current user
+    private func getCardOffset(_ geometry: GeometryProxy, id: Int) -> CGFloat {
+        let books = networkManager.books.results
+        return  CGFloat(books.count - removedIndices.count - 1 - id) * 20
+    }
+    
     var body: some View {
             if networkManager.loading {
                 Text("Loading ...")
             } else {
-                List(networkManager.books.results, id: \.title) { book in
-                    Text(book.title)
-                }
+                VStack {
+                    GeometryReader { geometry in
+                        ScrollView {
+                            VStack {
+                                ZStack {
+                                    ForEach(0..<networkManager.books.results.count) { i in
+                                        // show only top four cards
+//                                        if (i > networkManager.books.results.count - removedIndices.count - 5                                             && !removedIndices.contains(i)) {
+                                        // show only top card
+                                        if (i == networkManager.books.results.count - removedIndices.count - 1 && !removedIndices.contains(i)) {
+                                            CardView(id: i, book: networkManager.books.results[i], onRemove: { id, book, swipedRight in
+                                                removedIndices.append(id)
+                                                currentIndex -= 1
+                                                if (swipedRight) {
+                                                    bookList.append(book)
+                                                }
+
+                                                // load next
+                                                if (removedIndices.count == 20 && networkManager.hasNext()) {
+                                                    removedIndices.removeAll()
+                                                    networkManager.next()
+                                                    currentIndex = 19
+                                                    print(removedIndices)
+                                                    print(networkManager.books.results)
+                                                }
+                                            })
+                                            .animation(.spring())
+                                            .frame(width: self.getCardWidth(geometry, id: i),
+                                                     height: 400)
+                                            .offset(x: 0, y: self.getCardOffset(geometry, id: i))
+                                        }
+                                    }
+                                }
+                                Spacer()
+                                // todo: this clearly
+                                HStack {
+                                    Text("swipe left")
+                                    Spacer()
+                                    Text("swipe down")
+                                    Spacer()
+                                    Text("swipe right")
+                                }
+                            }
+                            // todo: add more info
+                            // todo: position so this isnt visible until scroll
+                            VStack {
+                                Text(networkManager.books.results[currentIndex].title)
+                            }
+                        }
+                    }
+                }.padding()
             }
     }
 }
