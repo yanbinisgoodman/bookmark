9663affb6338e4b69e1c0e83bc0c4aac584f64eb
elin6 <elin6@uw.edu>
2021-06-04 21:20:35 -0400

    Added everything from elizabeth branch

--- bookmark/bookmark/Views/ReadingListView.swift
+++ bookmark/bookmark/Views/ReadingListView.swift
@@ -2,56 +2,139 @@
 //  ReadingListView.swift
 //  bookmark
 //
-//  Created by Megan on 5/25/21.
+//  Created by Elizabeth 6/4/21
 //
 
 import SwiftUI
+import UIKit
 
 struct ReadingListView: View {
     @ObservedObject var networkManager = NetworkManager()
-
-    var body: some View {
-        
-//        let url = URL(string: "http://i.imgur.com/w5rkSIj.jpg")!
-//        let data = try? Data(contentsOf: url)
-//        imageView.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
-
-//        if let imageData = data {
-//            imageView = UIImage(data: imageData)
-//        }
-        
-//        var imageView : UIImageView
-//        imageView  = UIImageView(frame:CGRect(x:10, y:50, width:100, height:300));
-//        imageView.image = UIImage(named:"Test.jpeg")
-//        self.view.addSubview(imageView)
-//
-        
     
- 
+    var body: some View {
         if networkManager.loading {
             Text("Loading ...")
         } else {
-
-            List(networkManager.books.results, id: \.title) { book in
-                VStack(alignment: .leading) {
-                    Text(book.title)
-                        .fontWeight(.semibold)
-                    Text(book.author)
-                        .font(.subheadline)
-                        .foregroundColor(Color.gray)
+            NavigationView {
+                VStack {
+                    Text("Your Reading List")
+                        .font(.headline)
+                    
+                    NavigationLink(destination: ListView()) {
+                        Text("View List")
+                    }
+                    
+                    // OPTION 2: See all books
+                    ZStack {
+                        HStack {
+                            List {
+                                ForEach(0..<networkManager.books.results.count/2) { i in
+                                    BookView(id: i, book: networkManager.books.results[i])
+                                }
+                            }
+                            
+                            List {
+                                ForEach(networkManager.books.results.count/2..<networkManager.books.results.count) { i in
+                                    BookView(id: i, book: networkManager.books.results[i])
+                                }
+                            }
+                        }
+                    }
                 }
-                //Text(book.title + "\n" + book.author).fontWeight(.semibold)
             }
-
-
         }
     }
 }
 
-
+struct ListView: View {
+    @ObservedObject var networkManager = NetworkManager()
+    
+    var body: some View {
+        if networkManager.loading {
+            Text("Loading ...")
+        } else {
+            // OPTION 1: Get list of books
+            VStack {
+                Text("Reading List")
+                List(networkManager.books.results, id: \.title) { book in
+                    VStack(alignment: .leading) {
+                        Text(book.title)
+                            .fontWeight(.semibold)
+                        Text(book.author)
+                            .font(.subheadline)
+                            .foregroundColor(Color.gray)
+                    }
+                }
+            }
+        }
+    }
+}
 
 struct ReadingListView_Previews: PreviewProvider {
     static var previews: some View {
         ReadingListView()
     }
 }
+
+//        NavigationView {
+//            List {
+//                NavigationLink(destination: ListView()) {
+//                    Text("View List")
+//                }
+//            }
+//            .navigationBarTitle(Text("View Books"))
+//        }
+        
+//        if networkManager.loading {
+//            Text("Loading ...")
+//        } else {
+//            // OPTION 2: Get all books view
+//            ZStack {
+//                HStack {
+//                    List {
+//                        ForEach(0..<networkManager.books.results.count/2) { i in
+//                            BookView(id: i, book: networkManager.books.results[i])
+//                        }
+//                    }
+//                    List {
+//                        ForEach(networkManager.books.results.count/2..<networkManager.books.results.count) { i in
+//                            BookView(id: i, book: networkManager.books.results[i])
+//                        }
+//                    }
+//                }
+//            }
+//        }
+        
+//        if networkManager.loading {
+//            Text("Loading ...")
+//        } else {
+//            // OPTION 2: Get all books view
+//            ZStack {
+//                HStack {
+//                    List {
+//                        ForEach(0..<networkManager.books.results.count/2) { i in
+//                            BookView(id: i, book: networkManager.books.results[i])
+//                        }
+//                    }
+//                    List {
+//                        ForEach(networkManager.books.results.count/2..<networkManager.books.results.count) { i in
+//                            BookView(id: i, book: networkManager.books.results[i])
+//                        }
+//                    }
+//                }
+//            }
+//        }
+
+//        let url = URL(string: "http://i.imgur.com/w5rkSIj.jpg")!
+//        let data = try? Data(contentsOf: url)
+//        imageView.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+//
+//        if let imageData = data {
+//            imageView = UIImage(data: imageData)
+//        }
+//
+//        var imageView : UIImageView
+//        imageView  = UIImageView(frame:CGRect(x:10, y:50, width:100, height:300));
+//        imageView.image = UIImage(named:"Test.jpeg")
+//        self.view.addSubview(imageView)
+//
