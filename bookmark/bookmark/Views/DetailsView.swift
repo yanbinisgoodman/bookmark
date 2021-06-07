afaa991e88884326856deadde010976cd75e6e9f
rjemal <rawudajemal@Rawudas-MBP.T-mobile.com>
2021-06-07 16:53:03 -0700

    fixed rank & genre + removed age group

--- bookmark/bookmark/Views/DetailsView.swift
+++ bookmark/bookmark/Views/DetailsView.swift
@@ -11,31 +11,34 @@ import SwiftUI
 struct DetailsView: View {
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     @State private var textButton = "Remove from list"
+    
     var id: Int
     var book: Book
     var onDelete: (_ id: Int) -> Void
     
+    
+    
     @State private var pub = "Publisher: "
-    @State private var cost = "Price: "
+    @State private var isbn = "ISBN: "
     
     var body: some View {
         ScrollView {
             HStack {
                 VStack {
-                    Image("bookCover")
+                    Image("Color1")
                     .resizable()
-                    .position( x: 143, y: 20)
+                    .position( x: 143, y: -20)
                     .scaledToFit()
                     .edgesIgnoringSafeArea(.top)
                         .frame(width: 145, height: 500)
                     Text(pub)
-                        .offset(x: 40, y: -60)
+                        .offset(x: 40, y: -158)
                     Text(book.publisher ?? "n/a")
-                        .offset(x: 120, y: -80)
-                    Text(cost)
-                        .offset(x: 25, y: -80)
-                    Text(book.price)
-                        .offset(x: 80, y: -100)
+                        .offset(x: 120, y: -179)
+                    Text(isbn)
+                        .offset(x: 25, y: -170)
+                    Text(book.ranks_history.count > 0 ? book.ranks_history[0].primary_isbn13 : "n/a")
+                        .offset(x: 130, y: -190)
                 }
                 
                 
@@ -43,38 +46,40 @@ struct DetailsView: View {
                     Text(book.title)
                         .font(.system(size: 25))
                         .bold()
-                        .position(x: 75, y: 70)
+                        .position(x: 75, y: -20)
                     HStack {
                         VStack {
                             Text(book.author)
-                                .position(x: 70, y: 10)
+                                .position(x: 70, y: -100)
                             // todo: sub real values - rank is like not a thing? account for empty values
-                            Text("Rank")
-                                .offset(y:-40)
+                            Text(book.ranks_history.count > 0 ? "\(book.ranks_history[1].rank)": "n/a")
+                                .offset(y:-150)
                                 .font(.system(size: 15))
                                 .background(RoundedRectangle(cornerRadius: 20)
                                             .fill(Color.green)
                                             .frame(width: 140, height: 20)
-                                            .offset(x:-5, y: -40))
+                                            .offset(x:-5, y: -150))
                             
-                            Text("List_name")
-                                .offset(y:-30)
+                            Text(book.ranks_history.count > 0 ? book.ranks_history[0].list_name : "n/a")
+                                .offset(y:-130)
                                 .font(.system(size: 15))
                                 .background(
                             RoundedRectangle(cornerRadius: 20)
                                 .fill(Color.orange)
                                 .frame(width: 140, height: 20)
-                                .offset(x:-5, y: -30)
+                                .offset(x:-5, y: -130)
                             )
-                            Text("Age")
+                            /*Text(book.age_group ?? "n/a")
                                 .offset(y:-20)
-                                .font(.system(size: 15))
+                                .font(.system(size: 10))
                                 .background(
                             RoundedRectangle(cornerRadius: 20)
                                 .fill(Color.yellow)
                                 .frame(width: 140, height: 20)
                                 .offset(x:-5, y: -20)
+
                             )
+                             */
                                 
                         }
                     }
@@ -84,7 +89,7 @@ struct DetailsView: View {
                    // }
                     Text("Synopsis")
                         .bold()
-                        .position(x: -110, y: 20)
+                        .position(x: -47, y: -70)
                     
 
                     HStack{
@@ -102,12 +107,13 @@ struct DetailsView: View {
                                     
                                 
                             }
-                            .offset(x: -70, y: 75)
+                            .offset(x: -70, y: -5)
                             Text("Details")
                                 .bold()
-                                .offset(x: -205, y: -65)
+                                .offset(x: -205, y: -170)
+                            
                             Text(book.description ?? "coming soon")
-                            .position(x: 70, y: -150)
+                            .position(x: 70, y: -250)
                                 .frame(width: 300)
 
                         }
